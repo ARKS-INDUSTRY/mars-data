970aeaeba3550b9e0aa0786fb3ba7c7d3d4c62b6
RAKESH Dev <59530718+ARKS-INDUSTRY@users.noreply.github.com>
2022-02-25 13:32:57 +0000

    v2

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ base_url = "https://api.nasa.gov/mars-photos/api/v1/rovers"
 root = Tk()
 root.title("Mars Mission")
 root.geometry("550x600")
+
+
 def mars_images(camera, rover, sol):
    url = f"{base_url}/{rover}/photos?sol={sol}&camera={camera}&api_key={api}"
    data = requests.get(url)
@@ -23,33 +25,33 @@ def mars_images(camera, rover, sol):
 def save_img(img_list):
    count = 0
    for i in img_list:
-      count +=1
+      count += 1
       filename = f"data/{count}.jpeg"
       urllib.request.urlretrieve(i, filename)
+
 
 #imgl = mars_images("rhaz", "spirit","100")
 #save_img(imgl)
 heading = Label(root, text="Mars Mission! To the moon", font=("helvetica", 30))
 heading.pack(pady=10)
+
+
 def main_tab():
+   global sol, default_rover, default_cam, sol_entry
    main_frame = Frame(root)
    main_frame.pack()
    rover_list = ["opportunity", "curiosity", "spirit"]
    label_rover = Label(main_frame, text="Select rover: ")
    label_rover.pack(pady=10)
-   default = StringVar()
-   default.set("curiosity")
-   cam_selection = OptionMenu(main_frame,default, *rover_list)
+   default_rover = StringVar()
+   default_rover.set("curiosity")
+   cam_selection = OptionMenu(main_frame, default_rover, *rover_list)
    cam_selection.pack(pady=10)
    sol_label = Label(main_frame, text="enter a proper sol of the rover: ")
    sol_label.pack(pady=10)
    sol_entry = Entry(main_frame, width=30)
    sol_entry.pack(pady=10)
-   try:
-      sol = int(sol_entry.get())
-   except:
-      showerror("Sol error", "Please enter numbers\n")
-   rover = default.get()
+   rover = default_rover.get()
    if rover == rover_list[0]:
       cam_list = ["FHAZ", "RHAZ", "MAST", "CHEMCAM",
                   "MAHLI", "MARDI", "NAVCAM"]
@@ -57,7 +59,31 @@ def main_tab():
       cam_list = ["FHAZ", "RHAZ", "NAVCAM", "PANCAM", "MINITES"]
    else:
       cam_list = ["FHAZ", "RHAZ", "NAVCAM", "PANCAM", "MINITES"]
+   default_cam = StringVar()
+   default_cam.set("FHAZ")
+   cam_selection = OptionMenu(main_frame, default_cam, *cam_list)
+   cam_selection.pack(pady=10)
+   get_data_btn = Button(text="Get Data", color="white", bg="#1EE8A0", width=30)
+   get_data_btn.pack(pady=10) 
 
+def get_data():
+   try:
+      sol = int(sol_entry.get())
+   except:
+      showerror("Sol error", "Please enter numbers\n")
+   rover = default_rover.get()
+   cam = default_cam.get()
+   return rover, cam, sol
+    
+
+def result_tab():
+    data_list = []
+    #arvind code here
+
+    go_back_btn=Button(text="Go Back", color="white", bg="#1EE8A0", width=30)
+    go_back_btn.pack(pady=10)
+    #for go back btn
+    
 root.mainloop()
 
 
